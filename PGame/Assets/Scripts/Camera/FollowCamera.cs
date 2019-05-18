6e806b4e49fd2fe9153f3e14d1f66bbb2a3ee32e
KangNansi <larizuri713@gmail.com>
2019-05-18 21:24:22 +0200

    Camera Bounds

--- PGame/Assets/Scripts/Camera/FollowCamera.cs
+++ PGame/Assets/Scripts/Camera/FollowCamera.cs
@@ -8,6 +8,7 @@ public class FollowCamera : MonoBehaviour
     public float low;
     public Transform target;
     public Vector2 offset;
+    public Bounds bounds;
 
     private Vector2 targetViewportPosition;
 
@@ -24,7 +25,24 @@ public class FollowCamera : MonoBehaviour
         move.z = 0;
 
         currentPosition = currentPosition + move;
-        transform.position = PixelPerfect(currentPosition);
+        Vector3 newPosition = currentPosition;
+
+        Vector3 diff = newPosition - transform.position;
+        Vector3 lb = camera.ViewportToWorldPoint(Vector3.zero) + diff;
+        Vector3 rt = camera.ViewportToWorldPoint(Vector3.one) + diff;
+
+
+        float left = Mathf.Max(bounds.min.x - lb.x, 0);
+        float right = Mathf.Max(rt.x - bounds.max.x, 0);
+        float bottom = Mathf.Max(bounds.min.y - lb.y, 0);
+        float top = Mathf.Max(rt.y - bounds.max.y, 0);
+        
+
+        Debug.Log("Top: " + top + "\nBottom: " + bottom);
+
+        newPosition += new Vector3(left - right, bottom - top);
+
+        transform.position = PixelPerfect(newPosition);
     }
 
     private Vector3 PixelPerfect(Vector3 value)
---
+++ PGame/Assets/Scripts/Editor/Level/FollowCameraEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FollowCamera))]
+public class FollowCameraEditor : EditorBase<FollowCamera>
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+    }
+
+    private void OnSceneGUI()
+    {
+        Handles.color = Color.red;
+        Handles.DrawPolyLine(self.bounds.min,
+            new Vector3(self.bounds.max.x, self.bounds.min.y),
+            self.bounds.max,
+            new Vector3(self.bounds.min.x, self.bounds.max.y),
+            self.bounds.min);
+
+        Vector3 left = self.bounds.min + new Vector3(0, self.bounds.size.y / 2f);
+        left = Handles.FreeMoveHandle(left, Quaternion.identity, 0.1f, Vector3.one, Handles.DotHandleCap);
+        Vector3 right = self.bounds.max + new Vector3(0, -self.bounds.size.y / 2f);
+        right = Handles.FreeMoveHandle(right, Quaternion.identity, 0.1f, Vector3.one, Handles.DotHandleCap);
+
+        Vector3 down = self.bounds.min + new Vector3(self.bounds.size.x / 2f, 0);
+        down = Handles.FreeMoveHandle(down, Quaternion.identity, 0.1f, Vector3.one, Handles.DotHandleCap);
+        Vector3 up = self.bounds.max + new Vector3(-self.bounds.size.x / 2f, 0);
+        up = Handles.FreeMoveHandle(up, Quaternion.identity, 0.1f, Vector3.one, Handles.DotHandleCap);
+
+        self.bounds.min = new Vector3(Mathf.Floor(left.x), Mathf.Floor(down.y));
+        self.bounds.max = new Vector3(Mathf.Floor(right.x), Mathf.Floor(up.y));
+
+        self.bounds.extents = new Vector3(Mathf.Abs(self.bounds.extents.x),
+            Mathf.Abs(self.bounds.extents.y),
+            Mathf.Abs(self.bounds.extents.z));
+
+        Handles.color = Color.white;
+    }
+}
